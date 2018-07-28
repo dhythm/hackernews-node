1f27ea7f94dde5734e0dcb75e4ab8c62d6750d7f
Yuta Okada <yuta.okada.20@gmail.com>
2018-07-28 16:30:34 +0800

    Done Tutorial

---
+++ README.md
@@ -0,0 +1,11 @@
+# GraphQL - Node.js Tutorial
+https://www.howtographql.com/
+
+## Summary
+- graphql-yoga: GraphQL server work as an Application Layer
+- Prisma: database layer
+
+src/negerated/prisma.graphql の作成は`prisma deploy`にて実施
+```
+$ prisma deploy
+```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+const Subscription = require('./resolvers/Subscription')
+const Feed = require('./resolvers/Feed')
 
 let links = [{
     id: 'link-0',
@@ -8,14 +14,36 @@ let links = [{
 let idCount = links.length
 
 const resolvers = {
+    Query,
+    Mutation,
+    AuthPayload,
+    Subscription,
+    Feed,
+    /* remove in Chapter.7
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
+            feed: (root, args, context, info) => {
+            return context.db.query.links({},info)
+        },
+        // remove in Chapter.5
         feed: () => links,
         link: (root, args) =>  {
             const link = links.filter((item) => item.id == args.id)
             return link
         },
+        // end
     },
+    Mutation: {
+        post: (root, args, context, info) => {
+            return context.db.mutation.createLink({
+                data: {
+                    url: args.url,
+                    description: args.description,
+                },
+            }, info)
+        },
+    },
+    // remove in Chapter.5
     Mutation: {
         post: (root, args) => {
             const link = {
@@ -49,11 +77,21 @@ const resolvers = {
         id: (root) => root.id,
         description: (root) => root.description,
         url: (root) => root.url,
-    }
+    },
+    */
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://xxx.xxxxxx.xx/xxxxxxxxxxx-xxxxxx/xxxxxxxx/xxx',
+            secret: 'mysecret123',
+            debug: true,
+        }),
+    }),
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
